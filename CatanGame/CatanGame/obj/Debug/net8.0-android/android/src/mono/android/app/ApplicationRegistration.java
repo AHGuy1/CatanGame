573c328e73efdfc15a2767340a1b8ba0fd1ea465
AHGuy1 <ashaniguy1234@gmail.com>
2026-01-14 22:46:04 +0200

    Check

---
+++ CatanGame/CatanGame/obj/Debug/net8.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("CatanGame.Platforms.Android.MainApplication, CatanGame, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64755d4412decc4fe6.MainApplication.class, crc64755d4412decc4fe6.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
